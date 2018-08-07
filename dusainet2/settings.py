4532eb76546c8a480c64dac15c17afe6f782a732
Dusai <474377974@qq.com>
2018-08-07 22:03:12 +0800

    优化移动端ui(1)

--- dusainet2/settings.py
+++ dusainet2/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import pymysql         # mysql配置
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -103,10 +105,21 @@ WSGI_APPLICATION = 'dusainet2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dusainet2',
+        'USER': 'root',
+        'PASSWORD': 'a131930765',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
