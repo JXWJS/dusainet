8fc0aef8c6be11dc0ae54159c3ad34602c60abe4
Dusai <474377974@qq.com>
2018-08-13 14:26:35 +0800

    上线前常规优化3

--- dusainet2/settings.py
+++ dusainet2/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0tlwp6joab$q=jma33dcpflt=zl!7ry^l%3f96tj5djez$opo$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.dusaiphoto.com', '.dusai.net']
 
 # Application definition
 
@@ -159,6 +159,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'nginx_static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
