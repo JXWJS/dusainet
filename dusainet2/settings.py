e490571b66f19dc57208999173eb796de16666c8
Dusai <474377974@qq.com>
2018-08-20 15:25:46 +0800

    2018.08.20优化

--- dusainet2/settings.py
+++ dusainet2/settings.py
@@ -26,7 +26,7 @@ with open('secret_key.txt') as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.dusaiphoto.com', '.dusai.net']
 
